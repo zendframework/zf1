c633946fd4734daba428e5f63e8e43584a8e3f77
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-08-03 19:16:33 +0000

    [ZF-9854] Zend_Filter:
    - implemented usage of mb_internal_encoding
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@22790 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Filter/StringToUpper.php
+++ library/Zend/Filter/StringToUpper.php
@@ -57,6 +57,10 @@ class Zend_Filter_StringToUpper implements Zend_Filter_Interface
             $options = $temp;
         }
 
+        if (!array_key_exists('encoding', $options) && function_exists('mb_internal_encoding')) {
+            $options['encoding'] = mb_internal_encoding();
+        }
+
         if (array_key_exists('encoding', $options)) {
             $this->setEncoding($options['encoding']);
         }
