6bb98fc23b6e11b2a4d3dbfa6ce691c89914e30f
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2011-03-25 20:49:37 +0000

    ZF-11208 - Ensured that in situations where PHPUnit is not present, testing resource variables are available before using them causing an E_STRICT non-existent var message
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@23817 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Tool/Project/Provider/Controller.php
+++ library/Zend/Tool/Project/Provider/Controller.php
@@ -177,7 +177,7 @@ class Zend_Tool_Project_Provider_Controller
                 $response->appendContent('Would create a view script for the index action method at ' . $indexActionViewResource->getContext()->getPath());
             }
 
-            if ($testActionResource) {
+            if ($testingEnabled) {
                 $response->appendContent('Would create a controller test file at ' . $testActionResource->getParentResource()->getContext()->getPath());
             }
 
@@ -193,7 +193,7 @@ class Zend_Tool_Project_Provider_Controller
                 $indexActionViewResource->create();
             }
 
-            if ($testActionResource) {
+            if ($testingEnabled) {
                 $response->appendContent('Creating a controller test file at ' . $testActionResource->getParentResource()->getContext()->getPath());
                 $testActionResource->getParentResource()->create();
                 $testActionResource->create();
