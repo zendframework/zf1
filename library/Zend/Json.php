83079dd4a2de4dcc87f5d964d6ac3ff2770ea552
ezimuel <ezimuel@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2011-05-16 16:39:51 +0000

    Fix ZF-3257 (comment 16 May) recursive depth problem
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24039 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Json.php
+++ library/Zend/Json.php
@@ -277,7 +277,7 @@ class Zend_Json
             $childArray= array();
             foreach ($childrens as $child) {
                 $childname= $child->getName();
-                $element= self::_processXml($child,$ignoreXmlAttributes,$recursionDepth++);
+                $element= self::_processXml($child,$ignoreXmlAttributes,$recursionDepth+1);
                 if (array_key_exists($childname, $childArray)) {
                     if (empty($subChild[$childname])) {
                         $childArray[$childname]=array($childArray[$childname]);
