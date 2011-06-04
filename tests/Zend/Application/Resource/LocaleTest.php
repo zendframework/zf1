70cc1ba31bf409c6699d7ced0e3633573b0ac56a
freak <freak@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2011-06-04 01:26:08 +0000

    ZF-10381 Zend_App_Res_Locale Updating test to reflect old regressions
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24111 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Application/Resource/LocaleTest.php
+++ tests/Zend/Application/Resource/LocaleTest.php
@@ -117,6 +117,7 @@ class Zend_Application_Resource_LocaleTest extends PHPUnit_Framework_TestCase
     {
         $options = array(
             'default'      => 'kok_IN',
+            'force' => true
         );
 
         $resource = new Zend_Application_Resource_Locale($options);
@@ -146,7 +147,7 @@ class Zend_Application_Resource_LocaleTest extends PHPUnit_Framework_TestCase
         $resource = new Zend_Application_Resource_Locale($config);
         $resource->init();
         $backend = Zend_Locale::getCache()->getBackend();
-        $this->assertType('Zend_Cache_Backend_BlackHole', $backend);
+        $this->assertInstanceOf('Zend_Cache_Backend_BlackHole', $backend);
         Zend_Locale::removeCache();
     }
 
