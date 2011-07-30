6305b50e783357de3077c7584b6fd1f1d8963beb
adamlundrigan <adamlundrigan@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2011-07-30 01:32:25 +0000

    ZF-11451: Reverted patch applied in r24124
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24305 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Validate/EmailAddressTest.php
+++ tests/Zend/Validate/EmailAddressTest.php
@@ -554,17 +554,6 @@ class Zend_Validate_EmailAddressTest extends PHPUnit_Framework_TestCase
     {
         $this->assertTrue($this->_validator->getDomainCheck());
     }
-    
-    /**
-     * @group ZF-11222
-     * @group ZF-11451
-     */
-    public function testEmailAddressesWithTrailingDotInHostPartAreRejected()
-    {
-        $this->assertFalse($this->_validator->isValid('example@gmail.com.'));
-        $this->assertFalse($this->_validator->isValid('test@test.co.'));
-        $this->assertFalse($this->_validator->isValid('test@test.co.za.'));
-    }
 
     public function errorHandler($errno, $errstr)
     {
