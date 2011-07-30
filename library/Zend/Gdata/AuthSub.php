880b2560ef5933f5d6023c628e3da1c62ac69a51
mikaelkael <mikaelkael@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2011-07-30 09:56:03 +0000

    [ZF-2146]: stop output buffering in case of exception (fix problem in complete test execution)
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24315 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Gdata/AuthSub.php
+++ library/Zend/Gdata/AuthSub.php
@@ -169,6 +169,7 @@ class Zend_Gdata_AuthSub
         try {
             $response = $client->request('GET');
         } catch (Zend_Http_Client_Exception $e) {
+            ob_end_clean();
             require_once 'Zend/Gdata/App/HttpException.php';
             throw new Zend_Gdata_App_HttpException($e->getMessage(), $e);
         }
@@ -210,6 +211,7 @@ class Zend_Gdata_AuthSub
         try {
             $response = $client->request('GET');
         } catch (Zend_Http_Client_Exception $e) {
+            ob_end_clean();
             require_once 'Zend/Gdata/App/HttpException.php';
             throw new Zend_Gdata_App_HttpException($e->getMessage(), $e);
         }
