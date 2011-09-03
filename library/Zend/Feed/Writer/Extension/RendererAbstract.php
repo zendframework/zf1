92ae00d3327f03e0f72f9c68c8704398bacc84fa
ramon <ramon@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2011-09-03 19:27:21 +0000

    [ZF-11554] DOCUMENTATION
    - Fix typo docblock.
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24436 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Feed/Writer/Extension/RendererAbstract.php
+++ library/Zend/Feed/Writer/Extension/RendererAbstract.php
@@ -13,7 +13,7 @@
  * to padraic dot brady at yahoo dot com so we can send you a copy immediately.
  *
  * @category   Zend
- * @package    Zend_Feed_Writer_Entry_Rss
+ * @package    Zend_Feed_Writer
  * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version    $Id$
@@ -26,7 +26,7 @@ require_once 'Zend/Feed/Writer/Extension/RendererInterface.php';
 
  /**
  * @category   Zend
- * @package    Zend_Feed_Writer_Entry_Rss
+ * @package    Zend_Feed_Writer
  * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
