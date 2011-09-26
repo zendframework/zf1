764a4be81f8846346324be329cc9ffb63499da32
ezimuel <ezimuel@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2011-09-26 16:22:30 +0000

    ZF-10610: fixed the endpoint of the Amazon Sqs region
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24469 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Service/Amazon/Sqs/OfflineTest.php
+++ tests/Zend/Service/Amazon/Sqs/OfflineTest.php
@@ -20,6 +20,10 @@
  * @version    $Id$
  */
 
+require_once 'Zend/Service/Amazon/Sqs.php';
+require_once 'Zend/Service/Amazon/Sqs/Exception.php';
+require_once 'Zend/Http/Client/Adapter/Test.php';
+
 /**
  * @category   Zend
  * @package    Zend_Service_Amazon
@@ -32,12 +36,61 @@
  */
 class Zend_Service_Amazon_Sqs_OfflineTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Reference to Amazon service consumer object
+     *
+     * @var Zend_Service_Amazon_Sqs
+     */
+    protected $_amazon;
+    
+    /**
+     * Test based HTTP client adapter
+     *
+     * @var Zend_Http_Client_Adapter_Test
+     */
+    protected $_httpClientAdapterTest;
+    
     public function setUp()
     {
-        $this->markTestSkipped('No offline tests for Zend_Service_Amazon_Sqs');
+        //$this->markTestSkipped('No offline tests for Zend_Service_Amazon_Sqs');
+        
+        $this->_amazon= new Zend_Service_Amazon_Sqs('test','test');
+        
+        $this->_httpClientAdapterTest = new Zend_Http_Client_Adapter_Test();
+
+        $this->_amazon->getHttpClient()
+                      ->setAdapter($this->_httpClientAdapterTest);
     }
 
-    public function testNothing()
+    public function testSetRegion()
     {
+        $this->_amazon->setEndpoint('eu-west-1');
+        $endPoints= $this->_amazon->getEndpoints();
+        $this->assertEquals($this->_amazon->getEndpoint(),$endPoints['eu-west-1']);
+    }
+    
+    public function testSetNewRegion()
+    {
+        $this->_amazon->setEndpoint('foo');
+        $this->assertEquals($this->_amazon->getEndpoint(),'sqs.foo.amazonaws.com');
+    }
+    
+    public function testSetEmptyRegion()
+    {
+         $this->setExpectedException(
+            'Zend_Service_Amazon_Sqs_Exception',
+            'Empty region specified.'
+        );
+        $this->_amazon->setEndpoint('');
+    }
+    
+    public function testGetRegions()
+    {
+        $endPoints= array('us-east-1' => 'sqs.us-east-1.amazonaws.com',
+                                     'us-west-1' => 'sqs.us-west-1.amazonaws.com',
+                                     'eu-west-1' => 'sqs.eu-west-1.amazonaws.com',
+                                     'ap-southeast-1' => 'sqs.ap-southeast-1.amazonaws.com',
+                                     'ap-northeast-1' => 'sqs.ap-northeast-1.amazonaws.com');
+        $this->assertEquals($this->_amazon->getEndpoints(),$endPoints);
     }
 }
