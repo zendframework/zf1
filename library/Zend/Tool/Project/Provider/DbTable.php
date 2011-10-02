757e82b94148d8126e8bd0b8777c5765c50cf805
jaguarnet7 <jaguarnet7@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2011-10-02 21:20:02 +0000

    Complement docblock and documentation ZF-11767
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24489 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Tool/Project/Provider/DbTable.php
+++ library/Zend/Tool/Project/Provider/DbTable.php
@@ -143,6 +143,11 @@ class Zend_Tool_Project_Provider_DbTable
         }
     }
 
+    /**
+     * @param string $module        Module name action should be applied to.
+     * @param bool $forceOverwrite  Whether should force overwriting previous classes generated
+     * @return void 
+     */
     public function createFromDatabase($module = null, $forceOverwrite = false)
     {
         $this->_loadProfile(self::NO_PROFILE_THROW_EXCEPTION);
