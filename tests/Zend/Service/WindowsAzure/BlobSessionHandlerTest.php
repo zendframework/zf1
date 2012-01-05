0f107e8f48da919882b548951a650fbaf2bd11ce
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-01-05 20:35:02 +0000

    [2012] Update copyrights to 2012
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24593 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Service/WindowsAzure/BlobSessionHandlerTest.php
+++ tests/Zend/Service/WindowsAzure/BlobSessionHandlerTest.php
@@ -16,7 +16,7 @@
  * @package    Zend_Service_WindowsAzure
  * @subpackage UnitTests
  * @version    $Id$
- * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
@@ -45,7 +45,7 @@ require_once 'Zend/Service/WindowsAzure/TableSessionHandlerTest.php';
  * @package    Zend_Service_WindowsAzure
  * @subpackage UnitTests
  * @version    $Id$
- * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Zend_Service_WindowsAzure_BlobSessionHandlerTest extends Zend_Service_WindowsAzure_TableSessionHandlerTest
