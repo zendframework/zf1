b42a640271efd056e23235489675dfe638e26fc2
rob <rob@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-04-10 19:54:42 +0000

    Ensure that the static method that we're testing in Zend_Json_ServerTest is actually declared as a static
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24709 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Json/ServerTest.php
+++ tests/Zend/Json/ServerTest.php
@@ -85,9 +85,9 @@ class Zend_Json_ServerTest extends PHPUnit_Framework_TestCase
 
     public function testShouldBeAbleToBindCallback1ToServer()
     {
-        $this->server->addFunction(array('Zend_Json_ServerTest_Foo', 'bar'));
+        $this->server->addFunction(array('Zend_Json_ServerTest_Foo', 'staticBar'));
         $methods = $this->server->getFunctions();
-        $this->assertTrue($methods->hasMethod('bar'));
+        $this->assertTrue($methods->hasMethod('staticBar'));
     }
 
     public function testShouldBeAbleToBindCallback2ToServer()
@@ -462,10 +462,23 @@ class Zend_Json_ServerTest_Foo
      * @param  mixed $three
      * @return array
      */
-    public function bar($one, $two = 'two', $three = null)
+    static public function staticBar($one, $two = 'two', $three = null)
     {
         return array($one, $two, $three);
     }
+
+    /**
+     * Bar
+     *
+     * @param  bool $one
+     * @param  string $two
+     * @param  mixed $three
+     * @return array
+     */
+    public function bar($one, $two = 'two', $three = null)
+    {
+        return array($one, $two, $three);
+    }    
 
     /**
      * Baz
