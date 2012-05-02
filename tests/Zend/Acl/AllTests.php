8c63a405133e195757cb00e4d73e8b2f3c14b22f
digitalstruct <digitalstruct@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-05-02 04:41:55 +0000

    Fix ZF-12183: Add additional AllTests and a runalltests.php harness to run main ZF tests easily
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24740 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ tests/Zend/Acl/AllTests.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Acl
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id$
+ */
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Zend_Acl_AllTests::main');
+}
+
+require_once 'Zend/Acl/AclTest.php';
+
+/**
+ * @category   Zend
+ * @package    Zend_Acl
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @group      Zend_Acl
+ */
+class Zend_Acl_AllTests
+{
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Zend Framework - Zend_Acl');
+
+        $suite->addTestSuite('Zend_Acl_AclTest');
+
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Zend_Acl_AllTests::main') {
+    Zend_Acl_AllTests::main();
+}
+
