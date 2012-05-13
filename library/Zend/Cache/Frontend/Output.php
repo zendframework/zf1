a32e4a712e954a4298529c8f4d5c14748535c842
mabe <mabe@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-05-13 11:59:32 +0000

    ZF-12208: Zend_Cache_Frontend_Output - typo in docblock
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24800 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Cache/Frontend/Output.php
+++ library/Zend/Cache/Frontend/Output.php
@@ -55,7 +55,7 @@ class Zend_Cache_Frontend_Output extends Zend_Cache_Core
      *
      * @param  string  $id                     Cache id
      * @param  boolean $doNotTestCacheValidity If set to true, the cache validity won't be tested
-     * @param  boolean $echoData               If set to true, datas are sent to the browser if the cache is hit (simpy returned else)
+     * @param  boolean $echoData               If set to true, datas are sent to the browser if the cache is hit (simply returned else)
      * @return mixed True if the cache is hit (false else) with $echoData=true (default) ; string else (datas)
      */
     public function start($id, $doNotTestCacheValidity = false, $echoData = true)
