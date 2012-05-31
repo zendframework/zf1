2ebf1fbeb3beb6f60a1b86f6b2a80eb4b91474be
adamlundrigan <adamlundrigan@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-05-31 11:37:11 +0000

    ZF-12207: Remove references to Zend_Service_Simpy from Zend_Service_AllTests
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24838 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Service/AllTests.php
+++ tests/Zend/Service/AllTests.php
@@ -35,7 +35,6 @@ require_once 'Zend/Service/LiveDocx/AllTests.php';
 require_once 'Zend/Service/Nirvanix/AllTests.php';
 require_once 'Zend/Service/ReCaptcha/AllTests.php';
 require_once 'Zend/Service/ShortUrl/AllTests.php';
-require_once 'Zend/Service/Simpy/AllTests.php';
 require_once 'Zend/Service/SlideShareTest.php';
 require_once 'Zend/Service/StrikeIron/AllTests.php';
 require_once 'Zend/Service/Technorati/AllTests.php';
@@ -83,7 +82,6 @@ class Zend_Service_AllTests
         $suite->addTest(Zend_Service_Nirvanix_AllTests::suite());
         $suite->addTest(Zend_Service_ReCaptcha_AllTests::suite());
         $suite->addTest(Zend_Service_ShortUrl_AllTests::suite());
-        $suite->addTest(Zend_Service_Simpy_AllTests::suite());
         $suite->addTestSuite('Zend_Service_SlideShareTest');
         $suite->addTest(Zend_Service_StrikeIron_AllTests::suite());
         $suite->addTest(Zend_Service_Technorati_AllTests::suite());
