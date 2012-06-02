03e16c99984c018062ea67257faac7fba25b05fb
adamlundrigan <adamlundrigan@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-06-02 00:22:47 +0000

    ZF-11816: Use bindParam instead of oci_bind_by_name in Zend_Db_Statement::execute
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@24863 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Db/Statement/Oracle.php
+++ library/Zend/Db/Statement/Oracle.php
@@ -240,7 +240,7 @@ class Zend_Db_Statement_Oracle extends Zend_Db_Statement
             }
             $error = false;
             foreach (array_keys($params) as $name) {
-                if (!@oci_bind_by_name($this->_stmt, $name, $params[$name], -1)) {
+                if (!$this->bindParam($name, $params[$name], null, -1)) {
                     $error = true;
                     break;
                 }
