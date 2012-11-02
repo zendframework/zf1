10b3599d4a716e805548cc23d27b393a3fefc9ef
rob <rob@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-11-02 21:01:06 +0000

    Merged r25058 from trunk
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.12@25059 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/JsonTest.php
+++ tests/Zend/JsonTest.php
@@ -754,8 +754,28 @@ EOB;
 
     public function testEncodeObjectImplementingIterator()
     {
-        $this->markTestIncomplete('Test is not yet finished.');
+        $iterator = new ArrayIterator(array(
+            'foo' => 'bar',
+            'baz' => 5
+        ));
+        $target = '{"__className":"ArrayIterator","foo":"bar","baz":5}';
+
+        Zend_Json::$useBuiltinEncoderDecoder = true;
+        $this->assertEquals($target, Zend_Json::encode($iterator));
     }
+
+    /**
+     * @group ZF-12347
+     */
+    public function testEncodeObjectImplementingIteratorAggregate()
+    {
+        $iterator = new ZF12347_IteratorAggregate();
+        $target = '{"__className":"ZF12347_IteratorAggregate","foo":"bar","baz":5}';
+
+        Zend_Json::$useBuiltinEncoderDecoder = true;
+        $this->assertEquals($target, Zend_Json::encode($iterator));
+    }
+
 
     /**
      * @group ZF-8663
@@ -1010,5 +1030,20 @@ class Zend_Json_ToJsonWithExpr
         );
 
         return Zend_Json::encode($data, false, array('enableJsonExprFinder' => true));
+    }
+}
+
+/**
+ * @see ZF-12347
+ */
+class ZF12347_IteratorAggregate implements IteratorAggregate
+{
+    protected $array = array(
+        'foo' => 'bar',
+        'baz' => 5
+    );
+
+    public function getIterator() {
+        return new ArrayIterator($this->array);
     }
 }
