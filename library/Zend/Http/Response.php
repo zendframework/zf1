1e158aa35ff672564a74636c140ebc8309d6ed07
rob <rob@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-11-06 20:19:29 +0000

    merge r25078 to release-1.12
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.12@25079 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Http/Response.php
+++ library/Zend/Http/Response.php
@@ -641,7 +641,7 @@ class Zend_Http_Response
          * @link http://framework.zend.com/issues/browse/ZF-6040
          */
         $zlibHeader = unpack('n', substr($body, 0, 2));
-        if ($zlibHeader[1] % 31 == 0) {
+        if ($zlibHeader[1] % 31 == 0 && ord($body[0]) == 0x78 && in_array(ord($body[1]), array(0x01, 0x5e, 0x9c, 0xda)) {
             return gzuncompress($body);
         } else {
             return gzinflate($body);
