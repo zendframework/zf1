13a05ba98cdc717fecd3389940ed530d8a2c9c9c
rob <rob@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2012-11-07 20:29:29 +0000

    merge r25102 to release-1.12
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/release-1.12@25103 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Feed/Reader.php
+++ library/Zend/Feed/Reader.php
@@ -240,7 +240,7 @@ class Zend_Feed_Reader
                     $etag = $cache->load($cacheId.'_etag');
                 }
                 if ($lastModified === null) {
-                    $lastModified = $cache->load($cacheId.'_lastmodified');;
+                    $lastModified = $cache->load($cacheId.'_lastmodified');
                 }
                 if ($etag) {
                     $client->setHeaders('If-None-Match', $etag);
