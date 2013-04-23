80421e90c5577174b74088e0f6ceffe80d2f6e13
Adar Porat <hadar@mybuilding.org>
2013-04-23 10:24:09 -0400

    cleaning up install_dependencies.sh

--- tests/install_dependencies.sh
+++ tests/install_dependencies.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# Remove any existing phpunit packages
-phpenv rehash
-
 pear update-channels
 pear upgrade-all
+pear config-set auto_discover 1
 
+# remove any existing phpunit packages
 pear uninstall phpunit/PHPUnit
 pear uninstall phpunit/DbUnit
 pear uninstall phpunit/PHP_CodeCoverage
@@ -16,11 +15,7 @@ pear uninstall phpunit/PHPUnit_MockObject
 pear uninstall phpunit/PHPUnit_Selenium
 pear uninstall pear.symfony-project.com/YAML
 
-phpenv rehash
-
-# install phpunit 3.5
-echo "Installing PHPUnit 3.4"
-pear config-set auto_discover 1
+# install phpunit 3.4
 pear update-channels
 pear install pear.symfony-project.com/YAML-1.0.2
 pear install phpunit/PHPUnit_Selenium-1.0.1
@@ -31,5 +26,3 @@ pear install phpunit/PHP_CodeCoverage-1.0.2
 pear install phpunit/Text_Template-1.0.0
 pear install phpunit/DbUnit-1.0.0
 pear install pear.phpunit.de/PHPUnit-3.4.15
-
-phpenv rehash
