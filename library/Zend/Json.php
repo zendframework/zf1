f8b16631b47b20dfc50be791f2f73e227130cd60
Siad Ardroumli <siad.ardroumli@googlemail.com>
2013-06-30 17:06:05 +0200

    Update Json.php
    Handling null value without json_last_error.

--- library/Zend/Json.php
+++ library/Zend/Json.php
@@ -77,7 +77,9 @@ class Zend_Json
 
             // php < 5.3
             if (!function_exists('json_last_error')) {
-                if ($decode === null) {
+                if ($encodedValue === 'null') {
+                    return null;
+                } elseif ($decode === null) {
                     require_once 'Zend/Json/Exception.php';
                     throw new Zend_Json_Exception('Decoding failed');
                 }
