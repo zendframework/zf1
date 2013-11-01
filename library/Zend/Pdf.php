dda69c91c2734ea4f437d645527a2aa3efc5bc32
Stephen Orr <steve@stephenorr.co.uk>
2013-11-01 15:53:59 +0000

    Make changes as commented by @zenden2k
    I've updated the check here to be more cautious; no harm in checking to see if the fields exist before trying to use them!

--- library/Zend/Pdf.php
+++ library/Zend/Pdf.php
@@ -628,7 +628,8 @@ class Zend_Pdf
         
         foreach ($root->AcroForm->Fields->items as $field)
         {
-            if ( $field->FT->value == 'Tx' && $field->T !== null ) /* We only support fields that are textfields and have a name */
+            /* We only support fields that are textfields and have a name */
+            if ( $field->FT && $field->FT->value == 'Tx' && && $field->T && $field->T !== null ) 
             {
                 $this->_formFields[$field->T->value] = $field;
             }
