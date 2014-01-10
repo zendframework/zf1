583aaec3f16da72d38af8824046e9ff94ab73b60
Rob Allen <rob@akrabat.com>
2014-01-10 20:00:35 +0000

    Explicitly list all PHPUnit items to install to avoid circular dependency errors.

--- tests/install_dependencies.sh
+++ tests/install_dependencies.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
+# To install DbUnit, we have to install everything
+
+pear update-channels
+pear upgrade-all
 pear config-set auto_discover 1
-pear install pear.phpunit.de/DbUnit
+
+# remove any existing phpunit packages
+pear uninstall phpunit/PHPUnit
+pear uninstall phpunit/DbUnit
+pear uninstall phpunit/PHP_CodeCoverage
+pear uninstall phpunit/File_Iterator
+pear uninstall phpunit/Text_Template
+pear uninstall phpunit/PHP_Timer
+pear uninstall phpunit/PHPUnit_MockObject
+pear uninstall phpunit/PHPUnit_Selenium
+pear uninstall pear.symfony-project.com/YAML
+
+# Install
+pear config-set auto_discover 1
+pear install pear.symfony-project.com/YAML
+pear install pear.phpunit.de/PHPUnit_Selenium
+pear install phpunit/PHPUnit_MockObject
+pear install phpunit/PHP_Timer
+pear install phpunit/File_Iterator
+pear install phpunit/PHP_CodeCoverage
+pear install phpunit/Text_Template
+pear install phpunit/DbUnit
+pear install pear.phpunit.de/PHPUnit
