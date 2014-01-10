1be92994dfceba0db4f32a12096a6726062c1fc2
Rob Allen <rob@akrabat.com>
2014-01-10 21:52:55 +0000

    Improve install_dependencies.sh.

--- tests/install_dependencies.sh
+++ tests/install_dependencies.sh
@@ -7,18 +7,14 @@ pear channel-discover pear.symfony-project.com
 pear update-channels
 
 # remove any existing phpunit packages
-pear uninstall -n pear.phpunit.de/DbUnit
-pear uninstall -n pear.phpunit.de/PHPUnit_Selenium
 pear uninstall -n pear.phpunit.de/PHPUnit
+pear uninstall -n pear.phpunit.de/PHP_CodeCoverage
+pear uninstall -n pear.phpunit.de/PHPUnit_MockObject
 pear uninstall -n pear.phpunit.de/File_Iterator
 pear uninstall -n pear.phpunit.de/Text_Template
-pear uninstall -n pear.phpunit.de/PHP_CodeCoverage
 pear uninstall -n pear.phpunit.de/PHP_Timer
-pear uninstall -n pear.phpunit.de/PHPUnit_MockObject
 pear uninstall -n pear.symfony-project.com/YAML
 
 # Install
-pear config-set auto_discover 1
-pear install -f -o pear.phpunit.de/PHPUnit
-pear install -f -o pear.phpunit.de/PHPUnit_Selenium
-pear install -f -o pear.phpunit.de/DbUnit
+pear install -o pear.phpunit.de/PHPUnit
+pear install pear.phpunit.de/DbUnit
