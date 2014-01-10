9a8b96a2d652798efce2917b7f02269b0a6a5287
Rob Allen <rob@akrabat.com>
2014-01-10 20:31:54 +0000

    More install_dependencies.sh work.

--- tests/install_dependencies.sh
+++ tests/install_dependencies.sh
@@ -2,29 +2,24 @@
 
 # To install DbUnit, we have to install everything
 
+pear channel-discover pear.phpunit.de
+pear channel-discover pear.symfony-project.com
 pear update-channels
 pear upgrade-all
-pear config-set auto_discover 1
 
 # remove any existing phpunit packages
-pear uninstall pear.phpunit.de/PHPUnit
-pear uninstall pear.phpunit.de/DbUnit
-pear uninstall pear.phpunit.de/PHP_CodeCoverage
-pear uninstall pear.phpunit.de/File_Iterator
-pear uninstall pear.phpunit.de/Text_Template
-pear uninstall pear.phpunit.de/PHP_Timer
-pear uninstall pear.phpunit.de/PHPUnit_MockObject
-pear uninstall pear.phpunit.de/PHPUnit_Selenium
-pear uninstall pear.symfony-project.com/YAML
+pear uninstall -n pear.phpunit.de/DbUnit
+pear uninstall -n pear.phpunit.de/PHPUnit_Selenium
+pear uninstall -n pear.phpunit.de/PHPUnit
+pear uninstall -n pear.phpunit.de/File_Iterator
+pear uninstall -n pear.phpunit.de/Text_Template
+pear uninstall -n pear.phpunit.de/PHP_CodeCoverage
+pear uninstall -n pear.phpunit.de/PHP_Timer
+pear uninstall -n pear.phpunit.de/PHPUnit_MockObject
+pear uninstall -n pear.symfony-project.com/YAML
 
 # Install
 pear config-set auto_discover 1
-pear install pear.phpunit.de/PHPUnit
-pear install pear.symfony-project.com/YAML
-pear install pear.phpunit.de/PHPUnit_Selenium
-pear install pear.phpunit.de/PHPUnit_MockObject
-pear install pear.phpunit.de/PHP_Timer
-pear install pear.phpunit.de/File_Iterator
-pear install pear.phpunit.de/PHP_CodeCoverage
-pear install pear.phpunit.de/Text_Template
-pear install pear.phpunit.de/DbUnit
+pear install -f -o pear.phpunit.de/PHPUnit
+pear install -f -o pear.phpunit.de/PHPUnit_Selenium
+pear install -f -o pear.phpunit.de/DbUnit
