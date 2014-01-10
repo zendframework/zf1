d87454f5ff46a6cb4d6c32ab4b19aafabb16e573
Rob Allen <rob@akrabat.com>
2014-01-10 20:20:28 +0000

    Fix install_dependencies.sh

--- tests/install_dependencies.sh
+++ tests/install_dependencies.sh
@@ -7,24 +7,24 @@ pear upgrade-all
 pear config-set auto_discover 1
 
 # remove any existing phpunit packages
-pear uninstall phpunit/PHPUnit
-pear uninstall phpunit/DbUnit
-pear uninstall phpunit/PHP_CodeCoverage
-pear uninstall phpunit/File_Iterator
-pear uninstall phpunit/Text_Template
-pear uninstall phpunit/PHP_Timer
-pear uninstall phpunit/PHPUnit_MockObject
-pear uninstall phpunit/PHPUnit_Selenium
+pear uninstall pear.phpunit.de/PHPUnit
+pear uninstall pear.phpunit.de/DbUnit
+pear uninstall pear.phpunit.de/PHP_CodeCoverage
+pear uninstall pear.phpunit.de/File_Iterator
+pear uninstall pear.phpunit.de/Text_Template
+pear uninstall pear.phpunit.de/PHP_Timer
+pear uninstall pear.phpunit.de/PHPUnit_MockObject
+pear uninstall pear.phpunit.de/PHPUnit_Selenium
 pear uninstall pear.symfony-project.com/YAML
 
 # Install
 pear config-set auto_discover 1
+pear install pear.phpunit.de/PHPUnit
 pear install pear.symfony-project.com/YAML
 pear install pear.phpunit.de/PHPUnit_Selenium
-pear install phpunit/PHPUnit_MockObject
-pear install phpunit/PHP_Timer
-pear install phpunit/File_Iterator
-pear install phpunit/PHP_CodeCoverage
-pear install phpunit/Text_Template
-pear install phpunit/DbUnit
-pear install pear.phpunit.de/PHPUnit
+pear install pear.phpunit.de/PHPUnit_MockObject
+pear install pear.phpunit.de/PHP_Timer
+pear install pear.phpunit.de/File_Iterator
+pear install pear.phpunit.de/PHP_CodeCoverage
+pear install pear.phpunit.de/Text_Template
+pear install pear.phpunit.de/DbUnit
