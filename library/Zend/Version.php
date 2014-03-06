fc335da6fb7f0eaf49ad904cca54607cd34687b2
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-06 11:49:51 -0600

    [1.12.4] Release readiness
    - Added security announcements to README
    - Updated `Zend_Version::VERSION`

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.12.4dev';
+    const VERSION = '1.12.4';
 
     /**
      * The latest stable version Zend Framework available
