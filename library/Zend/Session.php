343de9d123fe4788caf5b8bce1d72c1bbca25c0b
Matthew Scragg <scragg@gmail.com>
2014-04-01 14:37:03 -0500

    Default session.hash_bits_per_character
    Change default session.hash_bits_per_character to 4.
    http://www.php.net/manual/en/session.configuration.php

--- library/Zend/Session.php
+++ library/Zend/Session.php
@@ -530,7 +530,7 @@ class Zend_Session extends Zend_Session_Abstract
 
         $hashBitsPerChar = ini_get('session.hash_bits_per_character');
         if (!$hashBitsPerChar) {
-            $hashBitsPerChar = 5; // the default value
+            $hashBitsPerChar = 4; // the default value
         }
         switch($hashBitsPerChar) {
             case 4: $pattern = '^[0-9a-f]*$'; break;
