4b59373d278af8f9b84ccb424ed7380460246931
Frank Brückner <dev@froschdesignstudio.de>
2015-01-10 21:58:49 +0100

    CS fixes in Zend_Application_Module_Autoloader

--- library/Zend/Application/Module/Autoloader.php
+++ library/Zend/Application/Module/Autoloader.php
@@ -54,40 +54,42 @@ class Zend_Application_Module_Autoloader extends Zend_Loader_Autoloader_Resource
     public function initDefaultResourceTypes()
     {
         $basePath = $this->getBasePath();
-        $this->addResourceTypes(array(
-            'dbtable' => array(
-                'namespace' => 'Model_DbTable',
-                'path'      => 'models/DbTable',
-            ),
-            'mappers' => array(
-                'namespace' => 'Model_Mapper',
-                'path'      => 'models/mappers',
-            ),
-            'form'    => array(
-                'namespace' => 'Form',
-                'path'      => 'forms',
-            ),
-            'model'   => array(
-                'namespace' => 'Model',
-                'path'      => 'models',
-            ),
-            'plugin'  => array(
-                'namespace' => 'Plugin',
-                'path'      => 'plugins',
-            ),
-            'service' => array(
-                'namespace' => 'Service',
-                'path'      => 'services',
-            ),
-            'viewhelper' => array(
-                'namespace' => 'View_Helper',
-                'path'      => 'views/helpers',
-            ),
-            'viewfilter' => array(
-                'namespace' => 'View_Filter',
-                'path'      => 'views/filters',
-            ),
-        ));
+        $this->addResourceTypes(
+            array(
+                'dbtable'    => array(
+                    'namespace' => 'Model_DbTable',
+                    'path'      => 'models/DbTable',
+                ),
+                'mappers'    => array(
+                    'namespace' => 'Model_Mapper',
+                    'path'      => 'models/mappers',
+                ),
+                'form'       => array(
+                    'namespace' => 'Form',
+                    'path'      => 'forms',
+                ),
+                'model'      => array(
+                    'namespace' => 'Model',
+                    'path'      => 'models',
+                ),
+                'plugin'     => array(
+                    'namespace' => 'Plugin',
+                    'path'      => 'plugins',
+                ),
+                'service'    => array(
+                    'namespace' => 'Service',
+                    'path'      => 'services',
+                ),
+                'viewhelper' => array(
+                    'namespace' => 'View_Helper',
+                    'path'      => 'views/helpers',
+                ),
+                'viewfilter' => array(
+                    'namespace' => 'View_Filter',
+                    'path'      => 'views/filters',
+                ),
+            )
+        );
         $this->setDefaultResourceType('model');
     }
 }
