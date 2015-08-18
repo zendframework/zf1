af4feebb61131d5e7610dd29d2894aa6d906ad76
Vasilis Tsaknis <vtsaknis@travelplanet24.com>
2015-08-18 19:00:42 +0300

    locale en_US short date format

--- tests/Zend/Locale/FunctionalTest.php
+++ tests/Zend/Locale/FunctionalTest.php
@@ -34,7 +34,7 @@ class Zend_Locale_FunctionalTest extends PHPUnit_Framework_TestCase
 ['sq_AL', '2015-04-05', '€1.234,56', 'e diel', 'e d', 'D', 'prill', 'Pri'],
 ['es_ES', '05/04/2015', '1.234,56 €', 'domingo', 'dom', 'd', 'abril', 'abr.'],
 ['pt_PT', '05/04/2015', '1 234,56 €', 'domingo', 'dom', 'd', 'Abril', 'Abr'],
-// ['en_US', '04/05/2015', '$1,234.56', 'Sunday', 'Sun', 'S', 'April', 'Apr'],
+['en_US', '04/05/2015', '$1,234.56', 'Sunday', 'Sun', 'S', 'April', 'Apr'],
 // ['en_NZ', '05/04/2015', 'NZ$1,234.56', 'Sunday', 'Sun', 'S', 'April', 'Apr'],
 // ['en_ZA', '05/04/2015', 'R1 234,56', 'Sunday', 'Sun', 'S', 'April', 'Apr'],
 // ['en_TP', '05/04/2015', '€1,234.56', 'Sunday', 'Sun', 'S', 'April', 'Apr'],
