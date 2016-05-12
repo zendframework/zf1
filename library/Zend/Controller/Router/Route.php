c19552b827d468aeebe9683afa3216981b9784cd
ephemeralsnow <ephemeralsnow@gmail.com>
2016-05-12 22:26:34 +0900

    Change array_key_exists() to isset()

--- library/Zend/Controller/Router/Route.php
+++ library/Zend/Controller/Router/Route.php
@@ -324,7 +324,7 @@ class Zend_Controller_Router_Route extends Zend_Controller_Router_Route_Abstract
             if (!array_key_exists($var, $return)) {
                 return false;
             } elseif ($return[$var] == '' || $return[$var] === null) {
-                if (array_key_exists($var, $this->_defaults)) {
+                if (isset($this->_defaults[$var])) {
                     // Empty variable? Replace with the default value.
                     $return[$var] = $this->_defaults[$var];
                 }
